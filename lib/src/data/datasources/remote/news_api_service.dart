3e959584205ac6001a9c02ed57c8a8c71a0b03c8
Komla David KOTOKOU <kotokou74@gmail.com>
2022-06-17 22:52:06 +0000

    changing the http service to a dio service

--- lib/src/data/datasources/remote/news_api_service.dart
+++ lib/src/data/datasources/remote/news_api_service.dart
@@ -2,17 +2,25 @@ import 'dart:convert';
 import 'package:daily_news/src/core/constants.dart';
 import 'package:daily_news/src/data/models/news_api_response.dart';
 import 'package:dio/dio.dart';
-import 'package:http/http.dart';
 
 
 NewsApiResponse newsApiResponseFromJson(String str) => NewsApiResponse.fromJson(json.decode(str));
 
 class NewsApiService {
+  late Dio _dio;
 
+  NewsApiService(){
+    _dio = Dio();
+  }
 
   Future<NewsApiResponse> fetchNewsArticle() async {
-  final response = await get(Uri.parse(url));
-  final newsResponse = newsApiResponseFromJson(response.body);
-  return newsResponse;
+    try{
+      final response = await _dio.get(url);
+      final newsResponse = NewsApiResponse.fromJson(response.data);
+      return newsResponse;
+    } on DioError catch(e){
+      print(e.toString());
+      throw UnimplementedError();
+    }
   }
 }
