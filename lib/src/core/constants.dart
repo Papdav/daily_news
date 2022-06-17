946678464bfa8a05e46ad23fb24299826a38044e
Komla David KOTOKOU <kotokou74@gmail.com>
2022-06-17 17:07:14 +0000

    structured of the project according to the concept of clean architecture

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,10 @@
+// BaseUrl
+const String url = "https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=$apiKey";
+
+// API key
+const String apiKey = "b0792c84a29543589c12ad23bfadb932";
+
+// PATH
+const String articleScreen = "/";
+const String articleDetailsScreen = "/articleDetailsScreen";
+const String articleSaved = "/articlesSaved";
